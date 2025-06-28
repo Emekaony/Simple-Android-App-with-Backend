de83bbf0de41921df0f5bd1577a14d20a84e8b2d
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-06-28 14:00:52 -0700

    working on forms now

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,24 +1,35 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.firstapp.ui.theme.FirstAppTheme
 
@@ -30,13 +41,65 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstAppTheme {
                 Scaffold { innerPadding ->
-                    TextComposable(
+                    TextEntryComposable(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+}
+
+@Composable
+fun TextEntryComposable(modifier: Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember {mutableStateOf("")}
+
+    Column (
+        modifier = Modifier.fillMaxSize().background(color = Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = {
+                Text("Enter your email...")
+            },
+            label = {
+              Text("Email")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = {
+                Text("Enter your password...")
+            },
+            label = {
+                Text("Password")
+            }
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        Button(
+            onClick = {
+                Log.d("LoginCredentials", "User logged in")
+            }
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TextEntryPreview() {
+    TextEntryComposable(Modifier)
 }
 
 @Composable
@@ -50,6 +113,7 @@ fun TextComposable(modifier: Modifier) {
     ) {
         Text (
             text = stringResource(R.string.hello_learners),
+            color = Color.Yellow,
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold
         )
