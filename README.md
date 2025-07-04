2a6b2cd962ad20a9f0e31db868dd4ad503712342
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-07-03 22:40:28 -0700

    Made login screen scrollable to make UX better
    Added a focus manager to be able to dismiss keyboard. Also made login screen scrollable

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Book project
 - End goal is to be able to volunteer as an Android instructor for codepath come Fall 2025!
 - Daa down, events up
+- I'm comfortable with laying UI out now. Next step will be to dig deeper and understand how I can
+    make it responsive and interactive. Also gonna learn to manage global state in the app.
--- app/src/main/java/com/example/book/LoginScreen.kt
+++ app/src/main/java/com/example/book/LoginScreen.kt
@@ -2,9 +2,14 @@ package com.example.book
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -12,12 +17,14 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,12 +36,26 @@ fun LoginScreen(){
     // state values across configuration changes
     var userName by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    val scrollState = rememberScrollState()
 
     // get the context
     val context = LocalContext.current
 
+    // get focus manager for keyboard dismissal
+    val focusManager = LocalFocusManager.current
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            // this will help us dismiss the keyboard when we press anywhere outside the text fields
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                focusManager.clearFocus()
+            }
+            .verticalScroll(scrollState)
+            .imePadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         // this spacedBy thing is a hack fr!
         verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically)
